fa82b043e6888142736103edd4efb0bba95dbe6e
Alexandr <ewqazxcds134@yandex.ru>
2026-03-25 19:39:10 +0300

    Переделанны message, conversation, participant.go

--- chat-service/internal/domain/message.go
+++ chat-service/internal/domain/message.go
@@ -2,4 +2,9 @@ package domain
 
 type Message struct {
 	//Message (id, conversation_id, sender_id, text, created_at)
+	id              	int
+	conversation_id 	int
+	sender_id       	int
+	text            	string
+	created_at      	string
 }
--- chat-service/internal/domain/participant.go
+++ chat-service/internal/domain/participant.go
@@ -1,5 +1,13 @@
 package domain
 
+type Role struct {
+	passenger bool
+	driver    bool
+}
+
 type Participant struct {
 	//Participant (conversation_id, user_id, role passenger/driver)
+	conversation_id 	int
+	user_id         	int
+	role            	Role
 }
